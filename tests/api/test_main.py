31bc409058174338015204bd7512353e611f292d
Timon Viola <44016238+timonviola@users.noreply.github.com>
2025-04-27 11:04:16 +0200

    test: add tests

--- tests/api/test_main.py
+++ tests/api/test_main.py
@@ -1,18 +1,23 @@
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
-
-app = FastAPI()
-
-
-@app.get("/")
-async def read_main():
-    return {"msg": "Hello World"}
-
+from fibonacci_api.main import app, Versions
+from fibonacci_api import crud
 
 client = TestClient(app)
 
 
-def test_read_main():
+def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+    content = response.json()
+    assert content["title"] == app.title
+    assert content["summary"] == app.summary
+    assert content["description"] == app.description
+
+
+def test_get_fibonacci_number():
+    fibonacci_val = 1
+    response = client.get(Versions.v1.value + f"/fibonacci/{fibonacci_val}")
+    assert response.status_code == 200
+    assert int(response.content.decode("utf-8")) == crud.calculate_fibonacci(
+        fibonacci_val
+    )
---
+++ tests/test_crud.py
@@ -0,0 +1,31 @@
+import pytest
+from fibonacci_api import crud
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [(0, 0), (1, 1), (2, 1), (3, 2), (4, 3), (5, 5), (6, 8), (12, 144)],
+)
+def test_calculate_fibonacci(test_input: int, expected: int):
+    assert crud.calculate_fibonacci(test_input) == expected
+
+
+def test_calculate_fibonacci_wrong_input():
+    with pytest.raises(ValueError):
+        crud.calculate_fibonacci(-1)
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [((1,0,5), (0,1,1)),
+     ((2,0,5), (0,2,2)),
+     ((5,0,2), (0,2,2)),
+     ((5,1,2), (2,4,2)),
+     ((5,2,2), (4,5,1)),
+     ((5,3,2), (6,5,0))
+    ],
+)
+def test_pagination(test_input, expected):
+    _range = crud.get_page(*test_input)
+    assert _range.start == expected[0]
+    assert _range.stop == expected[1]
+    assert len(_range) == expected[2]
